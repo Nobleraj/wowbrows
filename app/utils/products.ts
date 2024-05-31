fd186982b6fef99cd138e4cfdb4eb7c6b87f475f
Noble <nobleraj.raj@gmail.com>
2024-05-31 23:18:21 +0530

    implemented lauching soon page

---
+++ app/utils/products.ts
@@ -0,0 +1,108 @@
+import Academy1 from '../images/academy1.jpeg'
+import Academy2 from '../images/academy2.jpeg'
+import Academy3 from '../images/academy3.jpeg'
+
+import Academy4 from '../images/academy4.png'
+import Academy5 from '../images/academy5.png'
+import Academy6 from '../images/academy6.png'
+
+import Academy7 from '../images/academy7.png'
+import Academy8 from '../images/academy8.png'
+
+import CelebrityImage1 from '../images/celebrity1.jpg'
+import CelebrityImage2 from '../images/celebrity2.jpg'
+
+import EyeBrows1 from '../images/eyebrows1.png'
+import EyeBrows2 from '../images/eyebrows2.png'
+import EyeBrows3 from '../images/eyebrows3.png'
+import EyeBrows4 from '../images/eyebrows4.png'
+
+const Link = 'https://www.instagram.com/wowbrowsbydrpriya/'
+
+export const products = [
+    {
+      title: "Wow Brows",
+      link: Link,
+      thumbnail: Academy1,
+    },
+    {
+      title: "Hands on Training",
+      link: Link,
+      thumbnail:
+        Academy2,
+    },
+    {
+      title: "PMU Demonstration",
+      link: Link,
+      thumbnail:
+        Academy3,
+    },
+  
+    {
+      title: "Microblading",
+      link: Link,
+      thumbnail:
+      EyeBrows4,
+    },
+    {
+      title: "PMU Demonstration",
+      link:Link,
+      thumbnail:
+        Academy5,
+    },
+    {
+      title: "Hands on Training",
+      link: Link,
+      thumbnail:
+        Academy6,
+    },
+  
+    {
+      title: "Hands on Training",
+      link: Link,
+      thumbnail:
+        Academy7,
+    },
+    {
+      title: "PMU Academy",
+      link: Link,
+      thumbnail:
+        Academy8,
+    },
+    {
+      title: "Celebrity",
+      link: Link,
+      thumbnail:CelebrityImage1
+    },
+    {
+      title: "Celebrity",
+      link: Link,
+      thumbnail:CelebrityImage2
+    },
+    {
+      title: "Renderwork Studio",
+      link: Link,
+      thumbnail:EyeBrows1,
+    },
+  
+    {
+      title: "Combination Brows",
+      link: Link,
+      thumbnail:EyeBrows2,
+    },
+    {
+      title: "Nanoblading",
+      link: Link,
+      thumbnail:EyeBrows3,
+    },
+    {
+      title: "Combination Brows",
+      link: Link,
+      thumbnail:Academy4,
+    },
+    {
+      title: "E Free Invoice",
+      link: Link,
+      thumbnail:CelebrityImage1,
+    },
+  ];
